19e96b679d4d96fa0eb6c80dea147e05c3e76b22
Şenol Keskin <senolkeskin1537@gmail.com>
2019-10-19 17:36:22 +0300

    customer addcustomer ve infocustomer sayfaları eklendi ve güncellendi

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export * from "./Input";
 export * from "./Button";
-export * from "./Header";
 export * from "./ListItem";
 export * from "./AvatarItem";
+export * from "./Header";
+export * from "./HeaderLeft";
--- src/screens/styles.ts
+++ src/screens/styles.ts
@@ -4,8 +4,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#2B6EDC",
-    justifyContent: "center"
+    justifyContent: "center",
+    
   },
+  addCustomerContainer: {
+    flex: 1,
+    backgroundColor: "#2B6EDC",
+    justifyContent: "flex-start",
+  },
+
   headStyle: {
     paddingVertical: 30,
     justifyContent: "center",
@@ -36,7 +43,6 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(255, 255, 255, 0.4)',
     marginHorizontal: 5,
     paddingHorizontal: 45,
-    borderRadius: 2000,
     color: '#ffffff',
     marginBottom:10,
   },
@@ -67,12 +73,72 @@ const styles = StyleSheet.create({
     fontWeight: "900",
     marginHorizontal: 120,
     paddingVertical: 15,
+    
   },
 
   flatContainer:{
-    
     flex:1,
-  }
+  },
+
+  item: {
+    backgroundColor: '#B5BAEA',
+    padding: 20,
+    marginVertical: 2,
+    marginHorizontal: 8,
+  },
+  title: {
+    fontSize: 32,
+    color: "white",
+
+  },
+
+  row: {
+    elevation: 1,
+    borderRadius: 10,
+    backgroundColor: "#B5BAEA",
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 18,
+    paddingRight: 16,
+    marginLeft: 14,
+    marginRight: 14,
+    marginTop: 0,
+    marginBottom: 6,
+  },
+  row_cell: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  tikla: {
+    color: "#131843",
+    paddingLeft: 16,
+    flex: 0,
+    fontSize: 24,
+  },
+  musteri_adi: {
+    color: "#131843",
+    textAlignVertical: 'bottom',
+    includeFontPadding: false,
+    flex: 0,
+    fontSize: 20,
+  },
+  alt_bilgi: {
+    color: "#0A157A",
+    textAlignVertical: 'top',
+    includeFontPadding: false,
+    flex: 0,
+    fontSize: 10,
+  },
+
+
+
+  
+
+
   
   
 
